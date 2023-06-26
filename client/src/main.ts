ec4a8e3d1151171ac4234c5013ba0deed1269eee
kalyani <kalyani.nelluri54@gmail.com>
2023-06-26 20:35:25 +0100

    changed maint.ts code from switch to generics

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,74 +8,85 @@ import { State } from "./states/state";
 import { states } from "./states/states";
 import { clear, print, printNewLine, prompt } from "./ui/console.js";
 
-async function begin() {
-	clear();
-	print("👋 Welcome to our cool blog browser!");
-	await prompt("⌨️ Press [ENTER] to continue! 🕶️");
-	main();
+type StateAction<T> = (state: State) => Promise<T>;
+
+
+const stateActions: Record<string, StateAction<void>> = {
+	MENU: async (state) => {
+		clear();
+	  const newMenuOption = await showMenu();
+	  state.set(newMenuOption);
+	},
+	SEND_MESSAGE: async (state) => {
+		clear();
+	  const nextState = await sendMessage();
+	  state.set(nextState);
+	},
+	SHOW_POSTS: async (state) => {
+		clear();
+		const posts = await showAllPosts();
+		state.set(states.MENU);
+	  },
+	  SHOW_USERS: async (state) => {
+		clear();
+		const users = await showAllUsers();
+		state.set(states.MENU);
+	  },
+	  BROWSE_POSTS: async (state) => {
+		clear();
+		const post = await browsePosts();
+		state.set(states.MENU);
+	  },
+	  ADD_USER: async (state) => {
+		clear();
+		print("🏗️  This functionality has not been implemented!");
+		await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
+		state.set(states.MENU);
+	  },
+	  UNKNOWN: async (state) => {
+		clear();
+		print("😵 We have entered an unknown state.");
+		await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
+		state.set(states.MENU);
+	  },
+	  CABBAGE: async (state) => {
+		clear();
+		print("🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬", false);
+		print("🥬      CABBAGE MODE UNLOCKED     🥬", false);
+		print("🥬     Why did you want this?     🥬", false);
+		print("🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬", false);
+		await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
+		state.set(states.MENU);
+	  },
+	  default: async (state) => {
+		clear();
+		print(`🌋 😱 Uh-oh, we've entered an invalid state: "${state.get()}"`);
+		print("💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥", false);
+		print("💥 Crashing the program now...  💥", false);
+		print("💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥", false);
+		printNewLine();
+		exit(99);
+	  }
+  };
   
+
+async function begin() {
+  clear();
+  print("👋 Welcome to our cool blog browser!");
+  await prompt("⌨️ Press [ENTER] to continue! 🕶️");
+  main();
 }
 
 async function main() {
-	let state = new State();
-
+	const state = new State();
+  
 	while (true) {
-		switch (state.get()) {
-			case "MENU":
-				const newMenuOption = await showMenu();
-				state.set(newMenuOption);
-				break;
-			case "SEND_MESSAGE":
-				const nextState = await sendMessage();
-				state.set(nextState);
-				break;
-			case "SHOW_POSTS":
-				clear();
-				const posts = await showAllPosts();
-				state.set(states.MENU);
-				break;
-			case "SHOW_USERS":
-				clear();
-				const users = await showAllUsers();
-				state.set(states.MENU);
-				break;
-			case "BROWSE_POSTS":
-				clear();
-				const post = await browsePosts();
-				state.set(states.MENU);
-				break;
-			case "ADD_USER":
-				clear();
-				print("🏗️  This functionality has not been implemented!");
-				await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
-				state.set(states.MENU);
-				break;
-			case "UNKNOWN":
-				clear();
-				print("😵 We have entered an unknown state.");
-				await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
-				state.set(states.MENU);
-				break;
-			case "CABBAGE":
-				clear();
-				print("🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬", false);
-				print("🥬      CABBAGE MODE UNLOCKED     🥬", false);
-				print("🥬     Why did you want this?     🥬", false);
-				print("🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬🥬", false);
-				await prompt("⌨️ Press [ENTER] to return to the main menu! 🕶️");
-				state.set(states.MENU);
-				break;
-			default:
-				clear();
-				print(`🌋 😱 Uh-oh, we've entered an invalid state: "${state.get()}"`);
-				print("💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥", false);
-				print("💥 Crashing the program now...  💥", false);
-				print("💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥", false);
-				printNewLine();
-				exit(99);
-				break;
-		}
+	  const currentState = state.get();
+	  const action = stateActions[currentState] || stateActions.default;
+	  await action(state);
 	}
-}
+  }
+  
+  
 
 begin();
